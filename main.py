8ab9754d94c90748d26b1d6e44760962f6484408
Tejaswani <tejaswani.solankar@asipsolutions.com>
2025-09-15 08:18:45 +0530

    Add LangChain agent setup for job search functionality
    This commit introduces a new agent using LangChain to search for job postings on LinkedIn. The main function now invokes the agent to retrieve and print details of job listings in the Pune area from the past 24 hours, enhancing the application's capabilities.

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+load_dotenv()
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model='gpt-4')
+react_prompt = hub.pull("hwchase17/react")
+
+agent = create_react_agent(llm=llm,
+                           tools=tools,
+                           prompt=react_prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
+
 def main():
-    print("Hello from langchain-course!")
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job posting for an Langchain jobs in the Pune area on linkedin for past 24hrs and list their details."
+        }
+    )
+    print(result)
 
 
 if __name__ == "__main__":

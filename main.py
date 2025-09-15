cad11c5a9037b6fdf7fa029502e543866396cee9
Tejaswani <tejaswani.solankar@asipsolutions.com>
2025-09-15 15:03:53 +0530

    Add schemas and prompt for agent response handling
    This commit introduces a new `schemas.py` file defining the `AgentResponse` and `Source` models for structured agent responses. Additionally, a new `prompt.py` file is added, containing the `REACT_PROMPT_WITH_FORMAT_INSSTRUCTION` template to guide the agent's response format. The main application file is updated to integrate these new components, enhancing the agent's functionality and output formatting.

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-from dotenv import load_dotenv
-from langchain import hub
-from langchain.agents import AgentExecutor
-from langchain.agents.react.agent import create_react_agent
-from langchain_openai import ChatOpenAI
+from schemas import AgentResponse
+from prompt import REACT_PROMPT_WITH_FORMAT_INSSTRUCTION
 from langchain_tavily import TavilySearch
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnableLambda
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain.agents.react.agent import create_react_agent
+from langchain.agents import AgentExecutor
+from langchain import hub
+from dotenv import load_dotenv
 load_dotenv()
 
+
 tools = [TavilySearch()]
-llm = ChatOpenAI(model='gpt-4')
+llm = ChatOpenAI(model="gpt-4")
+
 react_prompt = hub.pull("hwchase17/react")
 
-agent = create_react_agent(llm=llm,
-                           tools=tools,
-                           prompt=react_prompt)
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instruction = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSSTRUCTION,
+    input_variables=["input", "agent_scratchpad", "tool_names", "tools"]
+).partial(format_instructions=output_parser.get_format_instructions())
+
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 chain = agent_executor
 
